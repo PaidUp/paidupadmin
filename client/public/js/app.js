4167c80a77e74228d445314e4c0e44a6cf0b3b8e
Diego Fortes <daigof@gmail.com>
2016-05-11 18:37:58 -0300

    alert system fix

--- client/public/js/app.js
+++ client/public/js/app.js
@@ -4,16 +4,12 @@
 angular.module('PaidUpAdmin', ['ui.bootstrap'])
 
   .controller('PaidUpCtrl', ['$rootScope', '$timeout', function ($rootScope, $timeout) {
-    $rootScope.GlobalAlertSystemAlerts = [
-      { type: 'danger', msg: 'This has a timeout it will dismiss in 2 seconds', dismissOnTimeout: 2000 },
-      { type: 'success', msg: 'This do not have a timeout it will stay until closed' },
-      { type: 'warning', msg: 'This do not have a timeout it will stay until closed' }
-    ]
     $timeout(function () {
-      $rootScope.GlobalAlertSystemAlerts.push({ type: 'info', msg: 'This shows hot to set alert progrmatically' })
-    }, 5000)
+      $rootScope.GlobalAlertSystemAlerts.push({ type: 'info', msg: 'This shows hot to set alert progrmatically', dismissOnTimeout: 1000 })
+    }, 2000)
   }])
   .run(['$rootScope', function ($rootScope) {
+    $rootScope.GlobalAlertSystemAlerts = []
     $rootScope.GlobalAlertSystemClose = function (index) {
       $rootScope.GlobalAlertSystemAlerts.splice(index, 1)
     }
