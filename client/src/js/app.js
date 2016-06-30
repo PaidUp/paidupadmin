59ec308b4aeb4e000b15418916a118191ecbfe6c
Ricardo Lara <riclara@gmail.com>
2016-06-29 19:15:48 -0500

    PU-9 Implement new PaidUp Admin Site.  configure build project

--- client/src/js/app.js
+++ client/src/js/app.js
@@ -1,8 +1,23 @@
 'use strict'
 
+var angular = require('angular')
+require('angular-ui-router')
+require('angular-resource')
+require('angular-sanitize')
+require('angular-cookies')
+require('angular-translate')
+require('angular-animate')
+//require('./vendor/angular-facebook')
+require('./vendor/alert.min')
+// require('./vendor/mixpanel')
+require('angular-ui-bootstrap')
+require('angulartics')
+require('angulartics-google-analytics')
+require('angular-ui-mask')
+require('angular-local-storage')
+
 /* global angular */
 angular.module('PaidUpAdmin', ['ui.bootstrap'])
-
   .controller('PaidUpCtrl', ['$rootScope', '$timeout', function ($rootScope, $timeout) {
     $timeout(function () {
       $rootScope.GlobalAlertSystemAlerts.push({ type: 'info', msg: 'This shows hot to set alert progrmatically', dismissOnTimeout: 1000 })
