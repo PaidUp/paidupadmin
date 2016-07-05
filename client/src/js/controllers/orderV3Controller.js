fd574b8c39957ec9341021ee28bf2c6b97afb1a7
Ricardo Lara <riclara@gmail.com>
2016-07-05 18:10:06 -0500

    startPU-9 Implement new PaidUp Admin Site.  start implement orders v3

--- client/src/js/controllers/orderV3Controller.js
+++ client/src/js/controllers/orderV3Controller.js
@@ -1,10 +1,27 @@
 'use strict';
 
-module.exports = ['$scope', '$state', '$rootScope', '$stateParams', '$location', 'SessionService', '$http', 'UserService',
-  function ($scope, $state, $rootScope, $stateParams, $location, SessionService, $http, UserService) {
+module.exports = ['$scope', 'CommerceService',
+  function ($scope, CommerceService) {
 
-    
-    
+    $scope.searchCriteria = '';
+    $scope.loading = false;
+    $scope.loader = '<i class="fa fa-circle-o-notch fa-spin"></i>';
+
+
+    $scope.search = function () {
+      $scope.loading = true;
+      $scope.accounts = []
+      $scope.orderSelected = null
+
+      CommerceService.orderSearch($scope.searchCriteria).then(function (result) {
+        console.log(result)
+        $scope.searchResult = result.body.orders
+        $scope.loading = false
+      }).catch(function (err) {
+
+        $scope.loading = false
+      })
+    }
 
   }
   
